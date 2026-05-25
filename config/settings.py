4faf554a5e8cc892f69b81c96a00c3dadc767825
Temur <Temuradilbekov468@gmail.com>
2026-05-25 15:30:53 +0500

    STATIC_ROOT added

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
